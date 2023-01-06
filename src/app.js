726eb7cbf2c3a3543aa09136d82f9ccb4228bb7f
omer-akkoca <omerakkoca1042@gmail.com>
2023-01-06 21:13:46 +0300

    navbar style with scroll

--- src/app.js
+++ src/app.js
@@ -2,19 +2,17 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home, AboutMe, PlacesVisited, BooksRead, Contact, NotFound } from "./pages"
 
-class App extends React.Component{
-    render(){
-        return(
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/about-me" element={<AboutMe/>}/>
-                <Route path="/places-visited" element={<PlacesVisited/>}/>
-                <Route path="/books-read" element={<BooksRead/>}/>
-                <Route path="/contact" element={<Contact/>}/>
-                <Route path="*" element={<NotFound/>}/>
-            </Routes>
-        )
-    }
+const App = () => {
+    return(
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about-me" element={<AboutMe/>}/>
+            <Route path="/places-visited" element={<PlacesVisited/>}/>
+            <Route path="/books-read" element={<BooksRead/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+            <Route path="*" element={<NotFound/>}/>
+        </Routes>
+    )
 }
 
 export default App;
--- src/layouts/Navbar/index.js
+++ src/layouts/Navbar/index.js
@@ -1,73 +1,83 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./style.css"
 import { Container } from "../../components"
 import { Link } from "react-router-dom";
 import { ABOUT_ME, BOOKS_READ, CONTACT, HOME, PLACES_VISITED } from "../../assets/images"
 
-class Navbar extends React.Component{
-    render(){
-        return(
-            <nav>
-                <Container>
-                    <div className="navbar-inner">
-                        <div className="logo">
-                            <Link to={"/"}>
-                                <h1>OA</h1>
-                            </Link>
-                        </div>
-                        <ul>
-                            <li>
-                                <Link to={"/"}>
-                                    <h1>Home</h1>
-                                    <div className="icon">
-                                        <img src={HOME} alt="home"/>
-                                    </div>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to={"/about-me"}>
-                                    <h1>About Me</h1>
-                                    <div className="icon">
-                                        <img src={ABOUT_ME} alt="home"/>
-                                    </div>
-                                </Link>
-                            </li>
-                            <li className="drop-down">
-                                <h1>My Interest</h1>
-                                <ul>
-                                    <li>
-                                        <Link to={"/places-visited"}>
-                                            <h1>Places Visited</h1>
-                                            <div className="icon">
-                                                <img src={PLACES_VISITED} alt="home" />
-                                            </div>
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link to={"/books-read"}>
-                                            <h1>Books Read</h1>
-                                            <div className="icon">
-                                                <img src={BOOKS_READ} alt="home" />
-                                            </div>
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <Link to={"/contact"}>
-                                    <h1>Contact</h1>
-                                    <div className="icon">
-                                        <img src={CONTACT} alt="home"/>
-                                    </div>
-                                </Link>
-                            </li>
-                        </ul>
+const Navbar = () => {
+
+    const navbar = useRef()
+
+    window.addEventListener("scroll",(e) => {
+        if (window.scrollY > 1) {
+            navbar.current.classList.add("scrolled-nav")
+        } else if (window.scrollY < 1) {
+            navbar.current.classList.remove("scrolled-nav")
+        }
+        console.log(e)
+    })
+
+    return (
+        <nav ref={navbar}>
+            <Container>
+                <div className="navbar-inner">
+                    <div className="logo">
+                        <Link to={"/"}>
+                            <h1>OA</h1>
+                        </Link>
                     </div>
-                    <div></div>
-                </Container>
-            </nav>
-        )
-    }
+                    <ul>
+                        <li>
+                            <Link to={"/"}>
+                                <h1>Home</h1>
+                                <div className="icon">
+                                    <img src={HOME} alt="home" />
+                                </div>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to={"/about-me"}>
+                                <h1>About Me</h1>
+                                <div className="icon">
+                                    <img src={ABOUT_ME} alt="home" />
+                                </div>
+                            </Link>
+                        </li>
+                        <li className="drop-down">
+                            <h1>My Interest</h1>
+                            <ul>
+                                <li>
+                                    <Link to={"/places-visited"}>
+                                        <h1>Places Visited</h1>
+                                        <div className="icon">
+                                            <img src={PLACES_VISITED} alt="home" />
+                                        </div>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to={"/books-read"}>
+                                        <h1>Books Read</h1>
+                                        <div className="icon">
+                                            <img src={BOOKS_READ} alt="home" />
+                                        </div>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <Link to={"/contact"}>
+                                <h1>Contact</h1>
+                                <div className="icon">
+                                    <img src={CONTACT} alt="home" />
+                                </div>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div></div>
+            </Container>
+        </nav>
+    )
 }
 
 export { Navbar }
