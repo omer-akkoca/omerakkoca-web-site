20d3823b5a9e32f8c38ea94a09d2f2e87a5103b4
omer-akkoca <omerakkoca1042@gmail.com>
2023-01-07 03:20:07 +0300

    not found page

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,13 +1,22 @@
 import React from "react";
+import "./style.css"
+import { Container } from "../../components";
+import { Link } from "react-router-dom";
 
-class NotFound extends React.Component{
-    render(){
-        return(
-            <div>
-                <h1>NOT FOUND</h1>
-            </div>
-        )
-    }
+const NotFound = () => {
+    return (
+        <section id="not-found">
+            <Container>
+                <div className="content">
+                    <h1>404</h1>
+                    <h2>I'm sorry. There is nothing here.</h2>
+                    <Link to={"/"}>
+                        <button className="go-back">Go Back to Home Page</button>
+                    </Link>
+                </div>
+            </Container>
+        </section>
+    )
 }
 
 export { NotFound }
