f45e7965274f3eefdaae6bbd8ac18b2a92faa33d
omer-akkoca <omerakkoca1042@gmail.com>
2023-01-09 17:34:42 +0300

    added header in contact page

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -4,8 +4,9 @@ import PLACES_VISITED from "./places-visited.png"
 import BOOKS_READ from "./books-read.png"
 import CONTACT from "./contact.png"
 import ABOUT_ME_HEADER from "./about-me-header.jpg"
+import CONTACT_HEADER from "./contact-header.jpg"
 
 export { 
     HOME, ABOUT_ME, PLACES_VISITED, BOOKS_READ, CONTACT,
-    ABOUT_ME_HEADER
+    ABOUT_ME_HEADER, CONTACT_HEADER
 }
