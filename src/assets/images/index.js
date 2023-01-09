e35f83272af2d4b0a3564a1fccc9b718364037e4
omer-akkoca <omerakkoca1042@gmail.com>
2023-01-09 20:24:44 +0300

    social media icons

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -6,6 +6,12 @@ import CONTACT from "./contact.png"
 import ABOUT_ME_HEADER from "./about-me-header.jpg"
 import CONTACT_HEADER from "./contact-header.jpg"
 
+import FACEBOOK from "./social-media/facebook.png"
+import INSTAGRAM from "./social-media/instagram.png"
+import TWITTER from "./social-media/twitter.png"
+import GITHUB from "./social-media/github.png"
+import LINKEDIN from "./social-media/linkedin.png"
+
 export { 
     HOME, ABOUT_ME, PLACES_VISITED, BOOKS_READ, CONTACT,
     ABOUT_ME_HEADER, CONTACT_HEADER
