4fdd9ba879677dda2a0276af7db4c276f0537698
omer-akkoca <omerakkoca1042@gmail.com>
2023-01-10 20:54:17 +0300

    contact page

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,8 +1,34 @@
 import React from "react";
 import "./style.css"
 import { Navbar } from "../../layouts";
-import { Footer, Header } from "../../components";
+import { Container, Footer, Header } from "../../components";
 import { CONTACT_HEADER } from "../../assets/images";
+import { IoLocation, IoMail } from "react-icons/io5"
+import { AiTwotonePhone } from "react-icons/ai"
+import { INSTAGRAM, FACEBOOK, TWITTER, GITHUB, LINKEDIN } from "../../assets/images"
+
+const social_media = [
+    {
+        url: "",
+        img: FACEBOOK
+    },
+    {
+        url: "",
+        img: INSTAGRAM
+    },
+    {
+        url: "",
+        img: TWITTER
+    },
+    {
+        url: "",
+        img: GITHUB
+    },
+    {
+        url: "",
+        img: LINKEDIN
+    }
+]
 
 const xd = "Buraya kadar neler yaptığımı nasıl biri olduğumu biraz olsun anlamışsınızdır.Şimdi eğer kafanızda herhangi bir proje var ise lütfen iletişim kurmayı unutmayınız."
 
@@ -12,7 +38,43 @@ class Contact extends React.Component{
             <section id="contact">
                 <Navbar />
                 <Header title={"contact"} image={CONTACT_HEADER} text={xd} />
-                <Footer/>
+                <div className="content-wrapper">
+                    <Container>
+                        <div className="contact-content">
+                            <div className="infos">
+                                <table>
+                                    <tbody>
+                                        <tr>
+                                            <td><IoLocation size={25} color={"#0d172a"} /></td>
+                                            <td><p>Sancaktepe - Istanbul / Turkey</p></td>
+                                        </tr>
+                                        <tr>
+                                            <td><IoMail size={25} color={"#0d172a"} /></td>
+                                            <td><p>omerakkoca1042@gmail.com</p></td>
+                                        </tr>
+                                        <tr>
+                                            <td><AiTwotonePhone size={25} color={"#0d172a"} /></td>
+                                            <td><p>(+90) 534 623 52 70</p></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div className="social-media">
+                                {
+                                    social_media.map((e,i) => (
+                                        <a key={i.toString()} href={e.url} target="_blank" rel="noreferrer">
+                                            <img
+                                                src={e.img}
+                                                alt="social"
+                                            />
+                                        </a>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                    </Container>
+                </div>
+                <Footer />
             </section>
         )
     }
