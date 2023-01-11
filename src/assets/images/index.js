2cb9c700bd25ce289a367b09e843cbc45c2b5347
omer-akkoca <omerakkoca1042@gmail.com>
2023-01-11 21:03:12 +0300

    places visited page

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -5,6 +5,7 @@ import BOOKS_READ from "./books-read.png"
 import CONTACT from "./contact.png"
 import ABOUT_ME_HEADER from "./about-me-header.jpg"
 import CONTACT_HEADER from "./contact-header.jpg"
+import PLACES_VISITED_HEADER from "./places-visited-header.jpg"
 
 import FACEBOOK from "./social-media/facebook.png"
 import INSTAGRAM from "./social-media/instagram.png"
@@ -14,6 +15,6 @@ import LINKEDIN from "./social-media/linkedin.png"
 
 export { 
     HOME, ABOUT_ME, PLACES_VISITED, BOOKS_READ, CONTACT,
-    ABOUT_ME_HEADER, CONTACT_HEADER,
+    ABOUT_ME_HEADER, CONTACT_HEADER, PLACES_VISITED_HEADER,
     FACEBOOK, INSTAGRAM, TWITTER, GITHUB, LINKEDIN
 }
--- src/pages/PlacesVisited/index.js
+++ src/pages/PlacesVisited/index.js
@@ -1,13 +1,34 @@
 import React from "react";
-import { Navbar } from "../../layouts"; 
+import "./style.css"
+import { Container, Footer, Header } from "../../components";
+import { Navbar } from "../../layouts";
+import { PLACES_VISITED_HEADER } from "../../assets/images";
 
+const xd = "Çalışmanın önemli olduğu kadar dünyayı gezip yeni insanlarla tanışmakta bir o kadar önemlidir. Hadi sizinle nereleri seyahet ettiğime bakalım."
 
 class PlacesVisited extends React.Component{
     render(){
         return (
             <section id="places-visited">
                 <Navbar />
-                <h1>PLACES VISITED</h1>
+                <Header title={"Places Visited"} image={PLACES_VISITED_HEADER} text={xd} />
+                <div className="visited-content-wrapper">
+                    <Container>
+                        <div className="visited-content">
+                            {
+                                [1,2,3,4,5,6,7,8,9].map(() => (
+                                    <div className="visited-card">
+                                        <img
+                                            src="https://firebasestorage.googleapis.com/v0/b/omer-akkoca.appspot.com/o/visits%2Fomer-2.jpg?alt=media&token=c9621ae1-2331-4215-8530-1ae07ec82029"
+                                            alt="photo"
+                                        />
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </Container>
+                </div>
+                <Footer/>
             </section>
         )
     }
