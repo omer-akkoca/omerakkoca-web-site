f259c9eac029ead93611290200aafba4d5fa419f
omer-akkoca <omerakkoca1042@gmail.com>
2023-01-15 21:54:57 +0300

    started to  books read page

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -6,6 +6,7 @@ import CONTACT from "./contact.png"
 import ABOUT_ME_HEADER from "./about-me-header.jpg"
 import CONTACT_HEADER from "./contact-header.jpg"
 import PLACES_VISITED_HEADER from "./places-visited-header.jpg"
+import BOOKS_READ_HEADER from "./books-read-header.jpg"
 
 import FACEBOOK from "./social-media/facebook.png"
 import INSTAGRAM from "./social-media/instagram.png"
@@ -21,7 +22,7 @@ import CLOSE from "./close.png"
 
 export { 
     HOME, ABOUT_ME, PLACES_VISITED, BOOKS_READ, CONTACT,
-    ABOUT_ME_HEADER, CONTACT_HEADER, PLACES_VISITED_HEADER,
+    ABOUT_ME_HEADER, CONTACT_HEADER, PLACES_VISITED_HEADER, BOOKS_READ_HEADER,
     FACEBOOK, INSTAGRAM, TWITTER, GITHUB, LINKEDIN,
     DETAILS, FULL_SCREEN, CANCEL,
     CLOSE
--- src/pages/BooksRead/index.js
+++ src/pages/BooksRead/index.js
@@ -1,12 +1,25 @@
 import React from "react";
+import "./style.css"
+import { BOOKS_READ_HEADER } from "../../assets/images";
+import { Container, Footer, Header } from "../../components";
 import { Navbar } from "../../layouts";
+
+const xd = '"Biz zevk için harcadığımız paradan fazla, kitap için sarfetmediğimiz müddetçe, bu ülke hiç bir zaman medeni bir ülke olamayacaktır" der Elbert Hubberd.'
 
 class BooksRead extends React.Component{
     render(){
         return(
             <section id="books-read">
                 <Navbar />
-                <h1>BOOKS READ</h1>
+                <Header title="books read" text={xd} image={BOOKS_READ_HEADER}/>
+                <div className="books-read-wrapper">
+                    <Container>
+                        <div className="books-read-content">
+
+                        </div>
+                    </Container>
+                </div>
+                <Footer/>
             </section>
         )
     }
