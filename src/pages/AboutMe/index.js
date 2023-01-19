b6f97c677c86a1be9ffc949d243978cdc5a630c8
omer-akkoca <omerakkoca1042@gmail.com>
2023-01-20 01:28:00 +0300

    about me software language setup

--- src/pages/AboutMe/index.js
+++ src/pages/AboutMe/index.js
@@ -1,17 +1,36 @@
 import React from "react";
-import { Footer, Header } from "../../components";
+import "./style.css"
+import { Container, Footer, Header } from "../../components";
 import { Navbar } from "../../layouts";
 import { ABOUT_ME_HEADER } from "../../assets/images";
+import { FirebaseClient } from "../../requests";
 
 const xd = "Welcome to my web site. Here is like my personal information page. You can see where did i work, what software language i know, my personal profile live etc. I hope you can find what you are looking for."
-//const xd = "Web sayfama hoşgeldin. Eminim ben kimim merak ediyorsunki bu sayfaya girdin :D. O zaman seni daha fazla bekletmiyorum. Hadi alttaki butona tıkla."
 
 class AboutMe extends React.Component{
+
+    state = {
+        software_languages: []
+    }
+
+    async componentDidMount(){
+        const { data } = await FirebaseClient.getFirestore({ collection_name: "language" })
+        this.setState({ software_languages: data })
+        console.log(data)
+    }
+
     render(){
         return (
             <section id="about-me">
                 <Navbar />
                 <Header title={"about me"} image={ABOUT_ME_HEADER} text={xd}/>
+                <div className="about-me-wrapper">
+                    <Container>
+                        <div className="about-content">
+
+                        </div>
+                    </Container>
+                </div>
                 <Footer/>
             </section>
         )
