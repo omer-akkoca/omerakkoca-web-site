2481b93a1e0a5c23779d4ff4589cf1a0fb9b981b
omer-akkoca <omerakkoca1042@gmail.com>
2023-01-20 00:51:48 +0300

    ordered firebase collection

--- src/requests/firebaseRequests.js
+++ src/requests/firebaseRequests.js
@@ -1,11 +1,13 @@
-import { collection, getDocs } from "firebase/firestore"
-import { firestore } from "../library/firebase";
+import { collection, getDocs, query, orderBy } from "firebase/firestore"
+import { firestore, } from "../library/firebase";
 
 class FirebaseRequests {
-    async getFirestore({ collection_name= "" }) {
+    async getFirestore({ collection_name = "", order_by = "id" }) {
         const data = []
-        const querySnapshot = await getDocs(collection(firestore, collection_name))
-        querySnapshot.forEach((doc) => {
+        const dataRef = collection(firestore, collection_name)
+        const ordered_data = query(dataRef, orderBy(order_by));
+        const dataSnapshot = await getDocs(ordered_data)
+        dataSnapshot.forEach((doc) => {
             let element = {
                 id: doc.id,
                 ...doc.data()
